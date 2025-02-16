390aa8e62cbb5764cfeb449cc1bebf165510e49a
r3d5ky <46210207+r3d5ky@users.noreply.github.com>
2025-02-17 00:08:28 +0300

    userscript: use multiple patch sources for resilience

--- userscript/twitchnosub.user.js
+++ userscript/twitchnosub.user.js
@@ -16,6 +16,12 @@
 (function () {
     'use strict';
 
+    const ivsPatchLinks = [
+        "https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub@master/src/patch_amazonworker.js",
+        "https://cdn.statically.io/gh/besuper/TwitchNoSub/master/src/patch_amazonworker.js",
+        "https://raw.githubusercontent.com/besuper/TwitchNoSub/master/src/patch_amazonworker.js"
+    ];
+
     // From vaft script (https://github.com/pixeltris/TwitchAdSolutions/blob/master/vaft/vaft.user.js#L299)
     function getWasmWorkerJs(twitchBlobUrl) {
         var req = new XMLHttpRequest();
@@ -25,19 +31,43 @@
         return req.responseText;
     }
 
-    const oldWorker = window.Worker;
-
-    window.Worker = class Worker extends oldWorker {
-        constructor(twitchBlobUrl) {
-            var workerString = getWasmWorkerJs(`${twitchBlobUrl.replaceAll("'", "%27")}`);
-
-            const blobUrl = URL.createObjectURL(new Blob([`
-                importScripts(
-                    'https://cdn.jsdelivr.net/gh/besuper/TwitchNoSub@master/src/patch_amazonworker.js',
-                );
-                ${workerString}
-            `]));
-            super(blobUrl);
+    async function overrideWorker() {
+        let ivsPatchLink = null;
+        // Attempt to find an accessible patch link
+        for (const link of ivsPatchLinks) {
+            try {
+                const fileResponse = await fetch(link, { method: "HEAD" });
+                if (fileResponse.ok) {
+                    ivsPatchLink = link;
+                    break;
+                }
+            } catch (error) {
+                console.error(`[TwitchNoSub] Error accessing patch link ${link}:`, error);
+            }
         }
+
+        if (!ivsPatchLink) {
+            console.error("[TwitchNoSub] All patch links are unavailable!");
+            return;
+        }
+        console.debug(`[TwitchNoSub] Loading patch from ${link}`);
+
+        const oldWorker = window.Worker;
+
+        window.Worker = class extends oldWorker {
+            constructor(twitchBlobUrl) {
+                var workerString = getWasmWorkerJs(`${twitchBlobUrl.replaceAll("'", "%27")}`);
+
+                const blobUrl = URL.createObjectURL(new Blob([`
+                    importScripts(
+                        '${ivsPatchLink}',
+                    );
+                    ${workerString}
+                `]));
+                super(blobUrl);
+            }
+        }
+        console.log('[TwitchNoSub] Worker overridden successfully!');
     }
+    overrideWorker();
 })();
